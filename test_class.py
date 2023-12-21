fed0ee6fc4c868b96a00ee22d574117d9e74a1bd
Jakub Bartczuk <bartczukkuba@gmail.com>
2023-12-21 17:24:59 +0100

    working tests

--- test_class.py
+++ test_class.py
@@ -7,12 +7,12 @@ from brml.variable import variable
 
 
 p = potential(1,1)
-print "var POTENTIAL.p created"
-print "p.variable = ", p.variables
-print "p.table = ", p.table
+print("var POTENTIAL.p created")
+print("p.variable = ", p.variables)
+print("p.table = ", p.table)
 
 v = variable('butler',['hehe', 'heihei'])
 # v = variable('butler',['murderer','not murderer'])
-print "var VARIABLE.v created"
-print "v.name = ", v.name
-print "v.domain = ", v.domain
+print("var VARIABLE.v created")
+print("v.name = ", v.name)
+print("v.domain = ", v.domain)
